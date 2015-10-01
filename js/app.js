df8874953a837abcc9547d85f7b0889ce644adf5
brissettdwayne <brissettdwayne@gmail.com>
2015-10-01 18:54:41 -0400

    2nd Draft

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
-	$("#about-link").click(function() {
-	$(".introduction").hide();
-    $(".projects").hide();
-    $(".about-me").show();
-    $("#about-link").addClass("active")
+    $("#about-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".about-me").offset().top
+    }, 700);
+    $("#about-link").addClass("active");
     $("#home").removeClass("active");
 });
-	$("#home").click(function() {
-	location.reload();
+    $("#home").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".introduction").offset().top
+    }, 700);
 });
-	$("#project-link").click(function() {
-	$(".introduction").hide();
-    $(".about-me").hide();
-    $(".projects").show();
+    $("#project-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".projects").offset().top
+    }, 700);
     $("#project-link").addClass("active");
     $("#home").removeClass("active");
     $("#about-link").removeClass("active")
